7e9ea771aa2413cee5ddaad7f2961886d056ba70
Fati Iseni <fati.iseni@pozitrongroup.com>
2023-10-17 11:32:56 +0200

    Added WebService (asmx) examples.

---
+++ HttpClientDemo.WebForms/WebServices/DemoWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="DemoWebService.asmx.cs" Class="HttpClientDemo.WebForms.WebServices.DemoWebService" %>
---
+++ HttpClientDemo.WebForms/WebServices/DemoWebService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using Nito.AsyncEx.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace HttpClientDemo.WebForms.WebServices
+{
+    /// <summary>
+    /// Summary description for DemoWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class DemoWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string RunOption63()
+        {
+            var response = new ClientNetFX6.Service63().GetStatusCode();
+            return response;
+        }
+
+        [WebMethod]
+        public IAsyncResult BeginRunAsync(string option, AsyncCallback callback, object state)
+        {
+            return ApmAsyncFactory.ToBegin<string>(ClientNetFX.Program.GetResponseAsync(option), callback, state);
+
+        }
+        [WebMethod]
+        public string EndRunAsync(IAsyncResult result)
+        {
+            return ApmAsyncFactory.ToEnd<string>(result);
+        }
+    }
+}
